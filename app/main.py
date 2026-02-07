ecd144e3d4b1f67821693df648d28756cba9bfe8
Kurhan Artem <kurhanartem@MacBook-Air-uzivatela-Kurhan.local>
2026-02-07 22:35:41 +0100

    Finished

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-class DeliveryDrone:
+class Cargo:
+    def __init__(self,weight: int) -> None:
+        self.weight = weight
+
+class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
@@ -20,7 +24,7 @@ class DeliveryDrone:
         return f"Robot: {self.name}, Weight: {self.weight}"
 
 
-class FlyingRobot(DeliveryDrone):
+class FlyingRobot(BaseRobot):
     def __init__(self, weight: int, name: str, coords: list = None) -> None:
         if coords is None:
             coords = [0, 0, 0]
@@ -32,3 +36,20 @@ class FlyingRobot(DeliveryDrone):
 
     def go_down(self, step: int = 1) -> None:
         self.coords[2] -= step
+
+
+class DeliveryDrone(FlyingRobot):
+    def __init__(self, name: str, weight: int, max_load_weight: int,
+        current_load: Cargo = None, coords: list = None) -> None:
+        super().__init__(name, weight, coords)
+        self.max_load_weight = max_load_weight
+        self.current_load = None
+        if current_load is not None:
+            self.hook_load(current_load)
+
+    def hook_load(self, cargo: Cargo) -> None:
+        if self.current_load is None and cargo.weight <= self.max_load_weight:
+            self.current_load = cargo
+
+    def unhook_load(self) -> None:
+        self.current_load = None
