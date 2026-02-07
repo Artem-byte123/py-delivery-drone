faa88559292d7f92f5b0a5588c96f2a46a1d44e1
Kurhan Artem <kurhanartem@MacBook-Air-uzivatela-Kurhan.local>
2026-02-07 22:59:02 +0100

    Finish

--- app/main.py
+++ app/main.py
@@ -29,8 +29,6 @@ class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list | None) -> None:
         if coords is None:
             coords = [0, 0, 0]
-
-
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
