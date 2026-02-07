1566108b8355491eb5bf7347aeb5a70fe5ce24ce
Kurhan Artem <kurhanartem@MacBook-Air-uzivatela-Kurhan.local>
2026-02-07 22:11:56 +0100

    Finish

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class DeliveryDrone:
-    def __init__(self, name: str, weight: int,  coords: list = None) -> None:
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
         self.coords = coords if coords is not None else [0, 0]
@@ -27,8 +27,8 @@ class FlyingRobot(DeliveryDrone):
 
         super().__init__(name, weight, coords)
 
-    def go_up(self, step=1) -> None:
+    def go_up(self, step: int = 1) -> None:
             self.coords[2] += step
 
-    def go_down(self, step=1) -> None:
+    def go_down(self, step: int = 1) -> None:
         self.coords[2] -= step
