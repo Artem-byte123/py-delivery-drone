0a77c1c2a652b4c237bc42f232c52801011284cf
Kurhan Artem <kurhanartem@MacBook-Air-uzivatela-Kurhan.local>
2026-02-07 22:15:21 +0100

    Finish

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ class FlyingRobot(DeliveryDrone):
         super().__init__(name, weight, coords)
 
     def go_up(self, step: int = 1) -> None:
-            self.coords[2] += step
+        self.coords[2] += step
 
     def go_down(self, step: int = 1) -> None:
         self.coords[2] -= step
